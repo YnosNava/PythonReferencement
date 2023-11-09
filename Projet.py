1e8c792927b91753c0f0c6b779797dbbc4eaf47b
YnosNava <150340303+YnosNava@users.noreply.github.com>
2023-11-09 14:05:00 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -4,36 +4,6 @@ import re
 import argparse
 
 
-def recuperer_html(url):
-    reponse = requests.get(url)
-    reponse.raise_for_status()
-    return reponse.text
-
-def supprimer_balises_html(texte_html):
-    soupe = BeautifulSoup(texte_html, 'html.parser')
-    return soupe.get_text()
-
-def extraire_valeurs_attribut(texte_html, balise, attribut):
-    soupe = BeautifulSoup(texte_html, 'html.parser')
-    return [element.get(attribut) for element in soupe.find_all(balise) if element.has_attr(attribut)]
-
-def compter_occurrences(texte):
-    texte = texte.lower()
-    texte = re.sub(r'[^\w\s]','', texte)
-    mots = texte.split()
-    occurrences = {}
-    for mot in mots:
-        occurrences[mot] = occurrences.get(mot, 0) + 1
-    return dict(sorted(occurrences.items(), key=lambda item: item[1], reverse=True))
-
-def supprimer_parasites(occurrences, parasites):
-    return {mot: occurrences[mot] for mot in occurrences if mot not in parasites}
-
-def charger_parasites(chemin_fichier):
-    with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
-        parasites = fichier.read().splitlines()
-    return parasites
-
 def audit_ref_seo(url, chemin_fichier_parasites):
     html = recuperer_html(url)
     texte_sans_html = supprimer_balises_html(html)
@@ -48,13 +18,44 @@ def audit_ref_seo(url, chemin_fichier_parasites):
     liens_sortants = [href for href in liens_href if not href.startswith(url)]
 
     return {
-        'Mots clés': list(mots_cles.items())[:3],  # Affiche les 3 premiers mots clés
+        'Mots clés': list(mots_cles.items())[:3],
         'Nombre de liens entrants': len(liens_entrants),
         'Nombre de liens sortants': len(liens_sortants),
         'Balises alt manquantes': sum(1 for alt in alt_images if not alt)
     }
+    
+def charger_parasites(chemin_fichier):
+    with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
+        parasites = fichier.read().splitlines()
+    return parasites
+    
+def extraire_valeurs_attribut(texte_html, balise, attribut):
+    soupe = BeautifulSoup(texte_html, 'html.parser')
+    return [element.get(attribut) for element in soupe.find_all(balise) if element.has_attr(attribut)]
 
-# Utilisation de la fonction audit_ref_seo
+
+def recuperer_html(url):
+    reponse = requests.get(url)
+    reponse.raise_for_status()
+    return reponse.text
+
+def compter_occurrences(texte):
+    texte = texte.lower()
+    texte = re.sub(r'[^\w\s]','', texte)
+    mots = texte.split()
+    occurrences = {}
+    for mot in mots:
+        occurrences[mot] = occurrences.get(mot, 0) + 1
+    return dict(sorted(occurrences.items(), key=lambda item: item[1], reverse=True))
+
+def supprimer_balises_html(texte_html):
+    soupe = BeautifulSoup(texte_html, 'html.parser')
+    return soupe.get_text()
+
+def supprimer_parasites(occurrences, parasites):
+    return {mot: occurrences[mot] for mot in occurrences if mot not in parasites}
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Effectuer un audit SEO simple sur une page web.')
     parser.add_argument('url', help='L\'URL de la page à analyser')
